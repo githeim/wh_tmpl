80232099829e47aef1c0ca67f37cec0c4bbb4356
githeim <githeim@nowhere.com>
2019-10-09 08:02:55 +0900

    export compile commands routine is added

--- .tmpl/build_system/cmake_template/cb.sh
+++ .tmpl/build_system/cmake_template/cb.sh
@@ -2,7 +2,9 @@
 echo Configuration and Build
 rm build -rf
 mkdir build
-cd build ; cmake .. -G Ninja && ninja -v -j14
+cd build ; cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -G Ninja \
+  && ln -s build/compile_commands.json .. \
+  && ninja -v -j14
 #cd build ; cmake .. && make -j
 
 
