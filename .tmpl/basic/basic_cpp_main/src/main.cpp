fb3f8823b000cb5869af9962f8b3afd9c9dbeff7
githeim <githeim@nowhere.com>
2019-10-20 07:06:08 +0900

    remove basic_cpp_main

--- .tmpl/basic/basic_cpp_main/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdlib.h>
-#include<stdio.h>
-
-int main(int argc, char *argv[])
-{
-  printf("Project ; WW_ProjectName_WW\n");
-  printf("Print Letter ; WW_PrintLetter_WW\n");
-  return 0;
-}
