982fe857e6e3ad09078ad3fbd76b4394d377db7b
githeim <>
2023-11-23 02:40:23 +0000

    Add ros2 launch/shutdown script

---
+++ .tmpl/ros2/ros2_pkg/launch_WW_ProjectName_WW.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo Launch WW_ProjectName_WW
+ros2 launch WW_ProjectName_WW WW_ProjectName_WW.launch.py
---
+++ .tmpl/ros2/ros2_pkg/shutdown_WW_ProjectName_WW.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo Shutdown WW_ProjectName_WW
+ros2 lifecycle set /WW_ProjectName_WW shutdown
