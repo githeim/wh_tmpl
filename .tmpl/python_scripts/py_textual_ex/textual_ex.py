01997587fd26fde74f41a19fd3a5919b6424b7e9
githeim <>
2023-12-04 06:58:56 +0000

    update py_textual_ex
    . Add popup example code
    . Add executions of shell routines

--- .tmpl/python_scripts/py_textual_ex/textual_ex.py
+++ .tmpl/python_scripts/py_textual_ex/textual_ex.py
@@ -1,17 +1,20 @@
 #!/bin/python3
+import subprocess
 
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer
 
 from textual.widgets import Button, Footer, Header, Static
 from textual.widgets import Label, ListItem, ListView, Log,RichLog,DirectoryTree
+from textual.screen import ModalScreen
+from textual.containers import Grid
 
 Options ={ 
     "touch A":    "create file A",
     "rm A":       "delete file A",
     "touch B":    "create file B",
-    "rm A":       "delete file A",
-    "cat ls > C": "create file C and write directory info to file C",
+    "rm B":       "delete file A",
+    "ls > C": "create file C and write directory info to file C",
     "rm C":       "delete file C"
           } 
 
@@ -35,8 +38,26 @@ class Evt_List(Static) :
 
     yield ListViewInstance
 
+# Popup for confirm 
+class ConfirmScreen(ModalScreen[int]):
+  """Screen with a dialog to quit."""
+  ConfirmText="Confirm?"
+  def compose(self) -> ComposeResult:
+    yield Grid(
+        Label(self.ConfirmText, id="question"),
+        Button("Yes", variant="error", id="Yes"),
+        Button("No", variant="primary", id="No"),
+        id="dialog"
+    )
 
-class WW_ProjectName_WW_App(App):
+  def on_button_pressed(self, event: Button.Pressed) -> None:
+    print("chk--------------------")
+    if   event.button.id == "Yes":
+      self.dismiss(0)
+    elif event.button.id == "No" : 
+      self.dismiss(1)
+
+class Textual_EX_App(App):
   """A Textual app example """
 
   BINDINGS = [
@@ -63,7 +84,6 @@ class WW_ProjectName_WW_App(App):
     yield Panel02
     yield Panel03
     yield Footer()
-
     
   def action_toggle_dark(self) -> None:
     """An action to toggle dark mode."""
@@ -81,6 +101,19 @@ class WW_ProjectName_WW_App(App):
     print("===================")
     print (event.item.label)
     print("===================")
+    def Chk_Confirm(Confirm: int) -> None:
+      print ("Confirm : ",Confirm)
+      if (Confirm == 0) :
+        # execute the item
+        subprocess.run(event.item.label, shell=True, universal_newlines=True,
+          stdout=subprocess.PIPE,
+          stderr=subprocess.STDOUT)
+        
+    Confirm= ConfirmScreen()
+    # Question for confirm - you can edit the question here
+    Confirm.ConfirmText="Execute this command?\n"+"$ "+event.item.label
+    self.push_screen(Confirm,Chk_Confirm)
+
 
 
 if __name__ == "__main__":
